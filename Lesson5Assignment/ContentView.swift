3558299d6168d945277ac179b365faab46d9f542
Sandy Gomez <sgomez1029@gmail.com>
2024-05-02 18:03:39 -0700

    Lesson 5 Assignment (Navigating foward and backward through views)

--- Lesson5Assignment/ContentView.swift
+++ Lesson5Assignment/ContentView.swift
@@ -9,15 +9,67 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            ZStack {
+                Color.blue.edgesIgnoringSafeArea(.all)
+                VStack (spacing: 25) {
+                    Text ("This is the first / initial View")
+                        .foregroundColor(.black)
+                    NavigationLink {
+                        SecondView()
+                    } label: {
+                        Text("Tap here to navigate to second view")
+                            .foregroundColor(.black)
+                    }
+                }
+                .navigationTitle("First View")
+            }
         }
-        .padding()
     }
 }
+    struct SecondView: View {
+        var body: some View {
+            ZStack {
+                Color.red.edgesIgnoringSafeArea(.all)
+                VStack (spacing: 25) {
+                    Text ("This is the second View")
+                    NavigationLink {
+                        ThirdView()
+                    } label: {
+                        Text("Tap here to navigate to third view")
+                            .foregroundColor(.black)
+                    }.navigationTitle("Second View")
+                }
+            }
+        }
+    }
+    struct ThirdView: View {
+        var body: some View {
+            ZStack {
+                Color.yellow.edgesIgnoringSafeArea(.all)
+                VStack (spacing: 25) {
+                    Text("This is the third View")
+                    NavigationLink {
+                        LastView()
+                    } label: {
+                        Text("Tap here to navigate to last view")
+                            .foregroundColor(.black)
+                    }.navigationTitle("Third view")
+                }
+            }
+        }
+    }
+    struct LastView: View {
+        var body: some View {
+            ZStack {
+                    Color.green.edgesIgnoringSafeArea(.all)
+                VStack {
+                    Text("This is the last view")
+                        .foregroundColor(.black)
+                }.navigationTitle("Last View")
+            }
+        }
+    }
 
 #Preview {
     ContentView()
